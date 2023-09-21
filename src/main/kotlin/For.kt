8aee8b7bd8ac0313aaf326376b072f951702f77e
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-09-21 13:40:11 -0400

    Added for loop sample

---
+++ src/main/kotlin/For.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val string = listOf("You", "don't", "know", "the", "power", "of", "the", "dark", "side")
+
+    for(i in string){
+        print("$i ")
+    }
+
+    println()
+
+}
