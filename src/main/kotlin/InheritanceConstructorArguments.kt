59f5e3e666055ed174cec07a5484a4da292ad9b6
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-10-12 19:46:01 -0400

    Added inheritance basics samples

---
+++ src/main/kotlin/InheritanceConstructorArguments.kt
@@ -0,0 +1,14 @@
+open class Dog(val name: String, val origin: String){
+
+    open fun speak() = println("The dog $name from $origin says: Ruf Ruf!")
+
+}
+
+class Cachorro(name: String) : Dog(name = name, origin = "Brazil")
+
+fun main(){
+
+    val dog: Dog = Cachorro("Tobi")
+    dog.speak()
+
+}
---
+++ src/main/kotlin/InheritanceParameterizedConstructor.kt
@@ -0,0 +1,20 @@
+open class Cat(val origin: String){
+
+    open fun speak() = println("A cat from $origin says: Meow!")
+
+}
+
+class Gato : Cat(origin = "Brazil"){
+
+    override fun speak() {
+        println("A cat from $origin says: Miau!")
+    }
+
+}
+
+fun main(){
+
+    val cat: Cat = Gato()
+    cat.speak()
+
+}
