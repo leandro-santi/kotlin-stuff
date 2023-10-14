442ac70c6d04df576b86ed8b0698838d6f90b67a
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-10-14 01:01:20 -0400

    Updating lambda functions sample

--- src/main/kotlin/LambdaFunctions.kt
+++ src/main/kotlin/LambdaFunctions.kt
@@ -16,10 +16,14 @@ fun main() {
 
     val upperCase6: (String) -> String = String::uppercase
 
+    val sum: (Int, Int) -> Int = {a: Int, b: Int -> a + b}
+
     println(upperCase1("hello"))
     println(upperCase2("hello"))
     println(upperCase3("hello"))
     println(upperCase5("hello"))
     println(upperCase6("hello"))
+
+    println(sum(1,2))
 
 }
