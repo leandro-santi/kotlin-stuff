db46b83c3c666954b3762c53240d847c40215bea
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-10-14 00:34:51 -0400

    Added high order functions sample

---
+++ src/main/kotlin/HighOrderFunctions.kt
@@ -0,0 +1,32 @@
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
+
+    return operation(x, y)
+
+}
+
+fun operation(): (Int) -> Int {
+
+    return ::square // '::' aciona uma função pelo nome
+
+}
+
+fun makeSum(x: Int, y: Int) = x + y
+
+fun makeSub(x: Int, y: Int) = x - y
+
+fun square(x: Int) = x * x
+
+fun main() {
+
+    val sumResult = calculate(4, 5, :: makeSum)
+    val mulResult = calculate(4, 5) { a, b -> a * b }
+    val subResult = calculate(4, 5, :: makeSub)
+
+    println("sumResult $sumResult, mulResult $mulResult, subResult $subResult")
+
+    println()
+
+    val func = operation()
+    println(func(2))
+
+}
